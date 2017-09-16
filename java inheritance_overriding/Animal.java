707e6fc8dabfc6931bbc10bfcc02251f925f0afe
Vikas Naik <vikasnaikpro@gmail.com>
2017-09-16 16:25:58 +0530

    learned java inheritance and overriding.

---
+++ java inheritance_overriding/Animal.java
@@ -0,0 +1,22 @@
+package practice;
+
+public class Animal {
+	
+	String name;
+	int legs;
+	int eyes;
+   
+	public Animal()
+	{
+		
+		
+	}
+	
+	public void speaking()
+	{
+		System.out.println("hello my name is "+name+" and i can ");
+	}
+	
+	
+
+}
